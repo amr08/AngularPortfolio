3490529a5553464e66bf757e231b9dbe11af7523
Andrea R <amrdev08@gmail.com>
2017-04-01 23:58:59 -0400

    fixed for deploy

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, function(){
+	console.log("app running on PORT " + PORT);
+});
