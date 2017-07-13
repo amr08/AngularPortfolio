bc226299a3c8b22faf7332d6915c25fa1e375ba3
Andrea R <amrdev08@gmail.com>
2017-07-12 23:44:18 -0400

    -v update

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -4,7 +4,7 @@ angular.module('newappApp')
   .controller('AboutCtrl', function ($scope) {
     $scope.tech = 
     	{
-    		SPA: "React.js, AngularJS",
+    		SPA: "React.js/Redux, AngularJS",
     		client: "HTML5/CSS3, jQuery, JavaScript, Bootstrap, Semantic UI, Materialize, AJAX/JSON, REST, Git, Responsive Web Design, UX/UI emphasis.",
     		server: "Node.js, Express.js, EJS, Handlebars, Cheerios",
     		db: "MongoDB, Mongoose, MySQL, Sequelize, Firebase"
